2a90e7bf133ac3da3a61ba69fb628539d3647211
Laerke <lavlis17@student.aau.dk>
2020-05-02 14:37:47 +0200

    Added links
    Added internal links between files, chat and settings

--- WIP/Chat_Page/Chat_Main.php
+++ WIP/Chat_Page/Chat_Main.php
@@ -10,8 +10,8 @@
 <div class="chat_main">
     <div class="top_menu">
         <h2 class="top_menu_item">Chat</h2>
-        <h2 class="top_menu_item">Files</h2>
-        <h2 class="top_menu_item">Settings</h2>
+        <h2 class="top_menu_item" href="Chat_Files.php">Files</h2>
+        <h2 class="top_menu_item" href="Chat_Settings.php">Settings</h2>
     </div>
     <div class="side_menu">
         <h2 class="users_online">Users Online</h2>
