67f4fce2573fc97cb18a5039b9ca774d20e63b60
Laerke <lavlis17@student.aau.dk>
2020-05-03 14:03:50 +0200

    Chat test added
    Added some test files to get the chat system up and running

---
+++ WIP/Chat_Test/chat_test_script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    //var from = null; 
+    //var start = 0; 
+
+    alert("jquery ready"); 
+   // from = prompt("Please enter your name:"); 
+   // alert("Hello " + from); 
+}); 
