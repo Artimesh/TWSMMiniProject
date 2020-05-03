283272627ef61abe647bbfc98dacddc7134eca3b
Artimesh <36537638+Artimesh@users.noreply.github.com>
2020-05-03 10:49:12 +0200

    Now checks if textfields are empty.

--- WIP/NicolaiTesting/register.php
+++ WIP/NicolaiTesting/register.php
@@ -4,33 +4,41 @@
     $password = "";
     $dbname = "chat_users";
 
-    //TODO:
-    //Check if fields are empty.
-    $Username = $_POST['uname'];
-    $Pw = $_POST['pw'];
-    $Email = $_POST['email'];
+    //Check if textfields are empty.
+    if("" != trim($_POST['uname']) and "" != trim($_POST['pw']) and "" != trim($_POST['email'])){
+        $Username = $_POST['uname'];
+        $Pw = $_POST['pw'];
+        $Email = $_POST['email'];
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+        try{       
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            // Check connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            //SQL query
+            $sql = "INSERT INTO users (Username, Pw, Email)
+            VALUES ('{$Username}', '{$Pw}', '{$Email}')";
 
-    //SQL query
-    $sql = "INSERT INTO users (Username, Pw, Email)
-    VALUES ('{$Username}', '{$Pw}', '{$Email}')";
+            //Tries to run the query.
+            if (mysqli_query($conn, $sql)) {
+                //successfully created user, then redirects to login page.
+                echo "New user created successfully";
+                header("refresh:3;url=//localhost/TWSMMiniProject/WIP/NicolaiTesting/nicolaiLogin.php");
+            } else {
+                //Prints error message and redirects to register page
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                header("refresh:3;url=//localhost/TWSMMiniProject/WIP/NicolaiTesting/nicolaiRegister.php");
+            }
 
-    //Tries to run the query.
-    if (mysqli_query($conn, $sql)) {
-        //successfully created user, then redirects to login page.
-        echo "New user created successfully";
-        header("refresh:3;url=//localhost/TWSMMiniProject/WIP/NicolaiTesting/nicolaiLogin.php");
+            mysqli_close($conn);
+        } catch(Exception $e){
+            echo $e;
+            header("refresh:3;url=//localhost/TWSMMiniProject/WIP/NicolaiTesting/nicolaiRegister.php");
+        }
     } else {
-        //Prints error message and redirects to register page
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        echo 'Please fill in all textfields...';
         header("refresh:3;url=//localhost/TWSMMiniProject/WIP/NicolaiTesting/nicolaiRegister.php");
     }
-
-    mysqli_close($conn);
 ?>
