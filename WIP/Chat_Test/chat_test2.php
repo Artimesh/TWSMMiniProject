3838c323edac6a21663f32f24b9855c22374fb5d
Artimesh <36537638+Artimesh@users.noreply.github.com>
2020-05-12 12:44:34 +0200

    maybe

---
+++ WIP/Chat_Test/chat_test2.php
@@ -0,0 +1,50 @@
+<?php
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "messages";
+    try{       
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        //SQL query
+
+        //Define items in the server
+        $result = array(); 
+        $Message = isset($_POST['Message']) ? $_POST['Message'] : null; 
+        $Sender = isset($_POST['Sender']) ? $_POST['Sender'] : null; 
+        if(!empty($Message) && !empty($Sender)){
+            $sql = "INSERT INTO messages (Message, Sender) 
+            VALUES ('{$Message}','{$Sender}')";
+            
+        }
+
+        //Tries to run the query.
+        if (mysqli_query($conn, $sql)) {
+            $result['send_status'] = mysqli_query($conn,$sql);
+            //successfully created user, then redirects to login page.
+            echo "Message saved successfully";
+            header("refresh:3;url=//localhost/TWSMMiniProject/WIP/LoginRegister/Login.html");
+        } else {
+            //Prints error message and redirects to register page
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            header("refresh:3;url=//localhost/TWSMMiniProject/WIP/LoginRegister/Register.html");
+        }
+
+        $start = isset($_GET['start']) ? intval($_GET['start']) : 0; 
+        $sql2 = ("SELECT * FROM messages WHERE ID >" . $start);
+        $items = mysqli_query($conn, $sql2); 
+        while($row = $items->fetch_assoc()){
+          $result['items'][] = $row; 
+        }
+
+        mysqli_close($conn);
+
+        echo json_encode($result); 
+    } catch(Exception $e){
+        echo $e;
+    }
+?>
