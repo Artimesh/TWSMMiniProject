346f8b5a74bf0ce2c0f3ee6d368e58bc4f161740
Artimesh <36537638+Artimesh@users.noreply.github.com>
2020-05-12 15:46:01 +0200

    I need a break now.

--- WIP/Chat_Test/chat_test_script.js
+++ WIP/Chat_Test/chat_test_script.js
@@ -1,32 +1,31 @@
     var from = null; //This is probably what needs to be changed to take information from the cookie storing the username 
     var start = 0; 
-    var url = "http://localhost/TWSMMiniProject/WIP/Chat_Test/chat_test.php"; 
+    var url = "http://localhost/TWSMMiniProject/WIP/Chat_Test/get_chat_messages.php";
+    var chat = "http://localhost/TWSMMiniProject/WIP/Chat_Test/chat_to_save.php";
 
 
 
 $(document).ready(function(){
 
-    from = prompt("Please enter your name:"); //Asking for a username - replace/remove for final version
-    load(); 
-    /*
+    //from = prompt("Please enter your name:"); //Asking for a username - replace/remove for final version
+    load();
     $('form').submit(function(e){
-        $.post(url, {
+        $.post(chat, {
             message: $('#message').val(), 
             from : from 
         });
         $('#message').val(''); 
         return false; 
    });
-   */ 
 }); 
 
 function load(){
-    $.get(url + '?start=' + start, function(result){
+    $.get(chat + '?start=' + start, function(result){
         if(result.items){
             result.items.forEach(item =>{
                 start = item.ID; 
                 $('#messages').append(renderMessage(item)); 
-            }); 
+            })
             $('#messages').animate({scrollTop: $('#messages')[0].scrollHeight}); 
         }; 
         load(); 
--- WIP/Chat_Test/chat_to_save.php
+++ WIP/Chat_Test/chat_to_save.php
@@ -1,12 +1,16 @@
 <?php
+    header('Access-Control-Allow-Origin: *'); 
+    header('Content-Type: application/json');
+
     $servername = "127.0.0.1";
     $username = "root";
     $password = "";
     $dbname = "chat_users";
 
-    $message = $_POST['message'];
+    //$message = $_POST['message'];
     $from = 'John';
 
+    $message = isset($_POST['message']) ? $_POST['message'] : null;
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
@@ -14,17 +18,22 @@
         echo "It's dead";
         die("Connection failed: " . mysqli_connect_error());
     }
+    //SQL query.
+    $sql = "INSERT INTO `chat` (`message`, `from`) VALUES ('{$message}', '{$from}')";
 
-    try{    
-
-        //SQL query.
-        $sql = "INSERT INTO `chat` (`message`, `from`) VALUES ('{$message}', '{$from}')";
-
-        //Runs query on database.
-        mysqli_query($conn, $sql);
-        
-    }catch(Exception $e){
-        echo $e;
+    //Runs query on database.
+    //mysqli_query($conn, $sql);
+    if(!empty($message) && !empty($from)){
+        //... insert data into the database
+        $result['send_status'] = mysqli_query($conn, $sql); 
     }
 
+    $start = isset($_GET['start']) ? intval($_GET['start']) : 0; 
+    $sql_select = "SELECT * FROM `chat` WHERE `id` >" . $start;
+    $items = mysqli_query($conn, $sql_select);
+    while($row = $items->fetch_assoc()){
+        $result['items'][] = $row; 
+    }
+
+    mysqli_close($conn);
 ?>
