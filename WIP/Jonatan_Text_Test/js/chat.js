aa1c1af8e27a2075c99a207f1d567b18d6c3d926
Jonatan Emil Simonsen <jsimon16@student.aau.dk>
2020-05-13 10:19:09 +0200

    Video done

--- WIP/Jonatan_Text_Test/js/chat.js
+++ WIP/Jonatan_Text_Test/js/chat.js
@@ -6,10 +6,35 @@ chat.fetchMessages = function (){
         url: 'ajax/chat.php',
         type: 'post',
         data: { method: 'fetch' },
-        success: function() {
+        success: function(data) {
             $('.chat .messages').html(data);
         }
     });
 }
 
+chat.throwMessage = function () {
+    if ($.trim(message).length != 0) {
+        $.ajax({
+            url: 'ajax/chat.php',
+            type: 'post',
+            data: { method: 'throw', message: message },
+            success: function(data) {
+                chat.fetchMessages();
+                chat.entry.val('');
+            }
+        });
+    }
+}
+
+//selector for entry text area
+chat.entry = $('.chat .entry');
+chat.entry.bind('keydown', function(e) {
+    // Enter has the keycode 13, so you submit text on enter
+    if(e.keyCode === 13 && e.shiftKey === false) {
+        chat.throwMessage($(this).val());
+        e.preventDefault();
+    }
+});
+
 chat.interval = setInterval(chat.fetchMessages, 5000);
+chat.fetchMessages();
